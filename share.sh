54284f1eebd0c34af4e3258394d8485ee9c15ceb
root <root@ex-npe-cntr-devops02-share.20vdl44sj4cuba4rbfzz3mkskf.ax.internal.cloudapp.net>
2018-10-28 10:29:16 +0000

    new_uplod_change

--- share.sh
+++ share.sh
@@ -2,9 +2,7 @@
 nginx & 
 while true
 do
-( df -h | grep -i exnpestrgdiag ) || {
-	mount -t cifs //exnpestrgdevopsdata.file.core.windows.net/vol-npe-dam-content-data [mount point] -o vers=3.0,username=exnpestrgdevopsdata,password=vKIHzPKFe6ubu9scpRJBHELc5Y2To5tZF8CavCWEp7zPAHH93bkPyZm9SpAmru7J3btt1GbdBnGeRYOCHJJWyQ==,dir_mode=0777,file_mode=0777,sec=ntlmssp
-}
-sleep 4g
+( df -h | grep -i exnpestrgdevopsdata ) || {	mount -t cifs //exnpestrgdevopsdata.file.core.windows.net/vol-npe-dam-content-data /usr/share/nginx/html/ -o vers=3.0,username=exnpestrgdevopsdata,password=vKIHzPKFe6ubu9scpRJBHELc5Y2To5tZF8CavCWEp7zPAHH93bkPyZm9SpAmru7J3btt1GbdBnGeRYOCHJJWyQ==,dir_mode=0777,file_mode=0777,sec=ntlmssp; }
+sleep 400
 done
 
